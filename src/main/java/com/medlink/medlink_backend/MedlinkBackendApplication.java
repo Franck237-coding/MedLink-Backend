ddc3ba8403465c90b051702f5876667d7326ce34
Itachi Uchiwa <edmond.sal@facsciences-uy1.cm>
2025-11-19 13:35:03 +0100

    Initial-commit

---
+++ src/main/java/com/medlink/medlink_backend/MedlinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medlink.medlink_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedlinkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medlink/medlink_backend/MedlinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medlink.medlink_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
